b51aefe88877eea68d2930ae8d8650d668a4551e
MatiasCordich <m.scordich@gmail.com>
2023-08-26 22:09:52 -0300

    Ejercicio 2 realizado

--- src/Ejercicio02.java
+++ src/Ejercicio02.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Ejercicio02 {
     public static void main(String[] args) throws Exception {
-        
 
-        
+        // ESCRIBIR UN ALGORITMO NECESARIO PARA CALCULAR Y MOSTRAR EL CUADRADO DE UN
+        // NUMERO. EL NUMERO DEBE SER MAYOR QUE CERO, EN CASO DE ERROR QUE APAREZCA EL
+        // MENSAJE DE "ERROR: EL NUMERO DEBE SER MAYOR QUE CERO "
+
+        int numero;
+        int cuadradoNumero;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero ");
+        numero = sc.nextInt();
+
+        if (numero > 0) {
+
+            cuadradoNumero = numero * numero;
+
+            System.out.println("El cuadrado de " + numero + " es: " + cuadradoNumero);
+
+        } else {
+            System.out.println("ERROR: El numero debe ser mayor que cero");
+        }
+
+        sc.close();
+
     }
 }
-
